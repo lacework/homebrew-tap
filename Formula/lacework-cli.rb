20e5175dacae34ba464df3dc2ae0b06fd35b5912
Darren Murray <darren.murray@lacework.net>
2021-01-05 16:44:17 +0000

    docs: Update Documentation
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- Formula/lacework-cli.rb
+++ Formula/lacework-cli.rb
@@ -1,7 +1,7 @@
 class LaceworkCli < Formula
   VERSION = "v0.2.12"
-  desc "Set of tools and libraries written in Go to interact with the Lacework platform"
-  homepage "https://www.lacework.com/running-with-lacework-cli"
+  desc "The Lacework Command Line Interface is a tool that helps you manage the Lacework cloud security platform"
+  homepage "https://github.com/lacework/go-sdk/wiki/CLI-Documentation"
   url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-darwin-amd64.zip"
   version "#{VERSION}"
   sha256 "30eccdd2317b09e3ad74c62e4c3c489dfd78a16e98d2afe359cbd0a4506025c8"
