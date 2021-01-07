94118b332c4b21df5899b6811ca492e88ca20acd
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-01-07 19:29:41 +0000

    feat: Determine os and arch (#3)
    * feat: Determine os and arch
    
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- Formula/lacework-cli.rb
+++ Formula/lacework-cli.rb
@@ -1,11 +1,33 @@
 class LaceworkCli < Formula
-  VERSION = "v0.2.12"
+  VERSION = "v0.2.13"
+  ARCH = RbConfig::CONFIG['host_cpu'].downcase
+
   desc "The Lacework Command Line Interface is a tool that helps you manage the Lacework cloud security platform"
   homepage "https://github.com/lacework/go-sdk/wiki/CLI-Documentation"
-  url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-darwin-amd64.zip"
   version "#{VERSION}"
-  sha256 "30eccdd2317b09e3ad74c62e4c3c489dfd78a16e98d2afe359cbd0a4506025c8"
   license "Apache-2.0"
+  
+  if OS.mac?
+    url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-darwin-amd64.zip"
+    sha256 "91e12d7e14ad9c3f780e9c73cc4bb18cb9d6151ad91678262daf6b326c2e03a8"
+  elsif OS.linux?
+    case ARCH
+      when "aarch64"
+        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm64.tar.gz"
+        sha256 "0d7f44c29cb2d850dc859e40f6151aa4979bb7438456968b0806ddc772a29da4"   
+      when /i\d86|x86|i86pc/
+        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-386.tar.gz"
+        sha256 "c3e0b866b8d4079c71f167ca0538c850e48c36532ba19a9673cdf6514142bfe3"
+      when /^arm/
+        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm.tar.gz"
+        sha256 "4cdc974952facd126a703858d7cb012cf393f7b394a590fe41f37e0abdb55e27"
+      when /amd64|x86_64|x64/
+        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-amd64.tar.gz"
+        sha256 "938f7f14a6d6bd3a7e6ab7eca29ccbe3719df0d8863f4eb5bb0fe69dad45c717"
+      else
+        print "The Lacework CLI formula does not support this Architecture #{ARCH}"
+      end
+  end
 
   bottle :unneeded
 
@@ -17,4 +39,3 @@ class LaceworkCli < Formula
     assert_match "lacework #{VERSION}", shell_output("#{bin}/lacework version", 2)
   end
 end
-
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# homebrew-lacework-cli
+<img src="https://techally-content.s3-us-west-1.amazonaws.com/public-content/lacework_logo_full.png" width="600">
+
+
+# Homebrew Lacework CLI
 A homebrew tap for the Lacework command line interface.
 
 ## Install
@@ -7,4 +10,17 @@ Add the lacework tap and install the lacework cli
 brew tap lacework/lacework-cli
 
 brew install lacework-cli
+```
+
+## Development
+When modifying this formula, use the following command to test local changes:
+```
+brew install --build-from-source Formula/lacework-cli.rb
+
+```
+Or, copy your changes to the homebrew formula directory and run brew install eg.
+```
+mv path/to/my-test-formula.rb /usr/local/Homebrew/Library/Taps/lacework/homebrew-lacework-cli/
+
+brew install my-test-formula
 ```
