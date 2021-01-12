f6f8eb8983f386e9a3464011670a98e0d4917a1a
Salim Afiune <afiune@lacework.net>
2021-01-12 10:17:43 +0100

    refactor: use native Homebrew helpers to detect OS (#6)
    I started reading about homebrew and there is already some abstraction
    we can use to detect the running OS and ARCH:
    
    https://www.rubydoc.info/github/Homebrew/brew/Hardware/CPU
    
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- Formula/lacework-cli.rb
+++ Formula/lacework-cli.rb
@@ -1,35 +1,31 @@
 class LaceworkCli < Formula
   VERSION = "v0.2.13"
-  ARCH = RbConfig::CONFIG['host_cpu'].downcase
-
   desc "The Lacework Command Line Interface is a tool that helps you manage the Lacework cloud security platform"
   homepage "https://github.com/lacework/go-sdk/wiki/CLI-Documentation"
   version VERSION
   license "Apache-2.0"
+  bottle :unneeded
   
   if OS.mac?
     url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-darwin-amd64.zip"
     sha256 "91e12d7e14ad9c3f780e9c73cc4bb18cb9d6151ad91678262daf6b326c2e03a8"
-  elsif OS.linux?
-    case ARCH
-      when "aarch64"
-        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm64.tar.gz"
-        sha256 "0d7f44c29cb2d850dc859e40f6151aa4979bb7438456968b0806ddc772a29da4"   
-      when /i\d86|x86|i86pc/
-        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-386.tar.gz"
-        sha256 "c3e0b866b8d4079c71f167ca0538c850e48c36532ba19a9673cdf6514142bfe3"
-      when /^arm/
-        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm.tar.gz"
-        sha256 "4cdc974952facd126a703858d7cb012cf393f7b394a590fe41f37e0abdb55e27"
-      when /amd64|x86_64|x64/
-        url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-amd64.tar.gz"
-        sha256 "938f7f14a6d6bd3a7e6ab7eca29ccbe3719df0d8863f4eb5bb0fe69dad45c717"
-      else
-        print "The Lacework CLI formula does not support this Architecture #{ARCH}"
-    end
   end
-
-  bottle :unneeded
+  if OS.linux? && Hardware::CPU.intel && !Hardware::CPU.is_64_bit?
+    url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-386.tar.gz"
+    sha256 "c3e0b866b8d4079c71f167ca0538c850e48c36532ba19a9673cdf6514142bfe3"
+  end
+  if OS.linux? && Hardware::CPU.intel && Hardware::CPU.is_64_bit?
+    url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-amd64.tar.gz"
+    sha256 "938f7f14a6d6bd3a7e6ab7eca29ccbe3719df0d8863f4eb5bb0fe69dad45c717"
+  end
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm.tar.gz"
+    sha256 "4cdc974952facd126a703858d7cb012cf393f7b394a590fe41f37e0abdb55e27"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm64.tar.gz"
+    sha256 "0d7f44c29cb2d850dc859e40f6151aa4979bb7438456968b0806ddc772a29da4"   
+  end
 
   def install
     prefix.install "lacework"
--- README.md
+++ README.md
@@ -1,28 +1,43 @@
 <img src="https://techally-content.s3-us-west-1.amazonaws.com/public-content/lacework_logo_full.png" width="600">
 
-
 # Homebrew Tap
 A homebrew tap for Lacework formulas.
 
 ## Lacework CLI Formula
-The formula for the Lacework command line interface installation.
+The formula for the Lacework command-line interface installation.
+
 ### Install
 Add the lacework tap and install the lacework cli
 ```
-brew tap lacework/tap
-
-brew install lacework-cli
+$ brew tap lacework/tap
+$ brew install lacework-cli
 ```
 
 ### Development
 When modifying this formula, use the following command to test local changes:
 ```
-brew install --build-from-source Formula/lacework-cli.rb
-
+$ brew install --build-from-source Formula/lacework-cli.rb
 ```
 Or, copy your changes to the homebrew formula directory and run brew install eg.
 ```
-mv path/to/my-test-formula.rb /usr/local/Homebrew/Library/Taps/lacework/homebrew-tap/
+$ mv path/to/my-test-formula.rb /usr/local/Homebrew/Library/Taps/lacework/homebrew-tap/
+$ brew install my-test-formula
+```
 
-brew install my-test-formula
+## License and Copyright
+
+Copyright 2020, Lacework Inc.
+
+```
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 ```
