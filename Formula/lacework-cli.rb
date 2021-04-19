ad79f5fcd9bf61d528235911431ab15fa8faf911
Lacework <ops+releng@lacework.net>
2021-04-19 13:20:05 +0000

    ci: update lacework-cli formula to v0.6.0

--- Formula/lacework-cli.rb
+++ Formula/lacework-cli.rb
@@ -1,5 +1,5 @@
 class LaceworkCli < Formula
-  VERSION = "v0.5.0".freeze
+  VERSION = "v0.6.0".freeze
   desc "Lacework command-line tool helps to manage the Lacework cloud platform"
   homepage "https://github.com/lacework/go-sdk/wiki/CLI-Documentation"
   version VERSION
@@ -8,23 +8,23 @@ class LaceworkCli < Formula
 
   if OS.mac?
     url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-darwin-amd64.zip"
-    sha256 "a65cbcd8e0acc23cb9b295e680ee66d082f2d8e6483675042349f99bc88eb90c"
+    sha256 "24a1083c0d6af82637c1bae8ea2c65fa634b769bbc97f0d79e297644e49b2fb0"
   end
   if OS.linux? && Hardware::CPU.intel? && !Hardware::CPU.is_64_bit?
     url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-386.tar.gz"
-    sha256 "558a93aadee548732ec7ad6b6d0ff8042a9b42fbad35291238c66ba5ce92fc96"
+    sha256 "4650aea0815061fb8ccc69ebea7ab59340fd7f5810763fbc846d34e2ea1d766d"
   end
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
     url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-amd64.tar.gz"
-    sha256 "cb7ffa6646098f1284e3ab002438b50df8b8599cce941b88161d675ad52ebc9a"
+    sha256 "8c09364dd37df3dcb08c145d57c08a611fe4abfc9496a0ab63930bda51e0196d"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm.tar.gz"
-    sha256 "2b8860d4ba95b2b97c5b521e8689fe1697de4a1b30a91e9b3dd3dcd7a4279588"
+    sha256 "0d91bf1ab3f8efa8be14b5758d1e3a40d32428f9a9d55f19ecf4f908ebc1f11a"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/lacework/go-sdk/releases/download/#{VERSION}/lacework-cli-linux-arm64.tar.gz"
-    sha256 "7ef0d41481521f05f3a28a7e5c24c286b947fa60cb142d71111023bfe47390b5"
+    sha256 "cf85efd06d32b3035e1cc1e12d37483d14d3ed60a1ae44be0760be2446d304cc"
   end
 
   def install
